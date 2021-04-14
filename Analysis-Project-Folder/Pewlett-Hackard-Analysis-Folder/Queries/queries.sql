e22f2dedf75ebce3c74f600e11f2a14dba6e6e5e
xumedT <sumedtamang@xumeds.lan>
2021-04-14 15:28:17 -0500

    updated quries

--- Analysis-Project-Folder/Pewlett-Hackard-Analysis-Folder/Queries/queries.sql
+++ Analysis-Project-Folder/Pewlett-Hackard-Analysis-Folder/Queries/queries.sql
@@ -85,13 +85,103 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 --  create a new table
-SELECT first_name, last_name 
-INTO retirement_info
+DROP TABLE emp_info
+SELECT emp_no, first_name, last_name, gender 
+INTO emp_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * from retirement_info;
+
+-- joining departments and dept_manager tables
+/*SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+*/
+
+-- joining retirement info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+
+SELECT
+COUNT (ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	s.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT dm.dept_no,
+	   d.dept_name,
+	   dm.emp_no,
+	   ce.last_name,
+	   ce.first_name,
+	   dm.from_date,
+	   dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+	INNER JOIN departments as d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce
+		ON (dm.emp_no = ce.emp_no);
+
+Select ce.emp_no,
+	   ce.first_name,
+	   ce.last_name,
+	   d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+
+-- Query that will only return the Sales and Development teams
+SELECT di.emp_no,
+	di.first_name,
+	di.last_name,
+	di.dept_name
+FROM dept_info as di
+WHERE di.dept_name IN ('Sales', 'Development');
+	
+select * from current_emp;
+
+
+
+
 
 
 
