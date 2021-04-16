6299c22e68ca8a105d2274d466be0bb7ce03d796
xumed001 <77937100+xumed001@users.noreply.github.com>
2021-04-16 18:47:03 -0500

    Add files via upload

--- Analysis-Project-Folder/Pewlett-Hackard-Analysis-Folder/Queries/Employee_Database_Challenge.sql
+++ Analysis-Project-Folder/Pewlett-Hackard-Analysis-Folder/Queries/Employee_Database_Challenge.sql
@@ -7,6 +7,15 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
+/*-- born 1952 - 1955 & still working 
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+FROM employees AS e
+JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (ti.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+*/
 
 /*-- NON Unique 
 SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
